cd5f9c9a89c1749df909ac6520590953dd658cb5
Tom Thompson <tom.thompson@unity3d.com>
2020-12-09 12:41:58 -0500

    modify tutorial copy

--- Assets/Match3/Scripts/Match3GameSimulation.cs
+++ Assets/Match3/Scripts/Match3GameSimulation.cs
@@ -1,8 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
-//TODO: 0. Import Unity Game Simulation package by uncommenting the line below
-//using Unity.Simulation.Games;
+// TODO: Game Simulation uses the following namespace: Unity.Simulation.Games
+// Reference: https://docs.unity3d.com/Packages/com.unity.simulation.games@0.4/manual/ImplementationGuide.html
 
 public class Match3GameSimulation : MonoBehaviour {
 
@@ -12,61 +11,43 @@ public class Match3GameSimulation : MonoBehaviour {
     private LevelSO levelSO;
 
     private void Awake() {
-        // TODO: 1. Load parameters for grid search
-        // Reference: https://docs.unity3d.com/Packages/com.unity.simulation.games@0.4/manual/ImplementationGuide.html#step-3-load-parameters-for-grid-search
-        // To load the parameters, call GameSimManager.Instance.FetchConfig(Action<GameSimConfigResponse>) here.
-        // Action<GameSimConfigResponse> is a call back method, and we will name it OnFetchConfig, which will be implemented in TODO: 2a.
+        // TODO: Load parameters for grid search
+        // GameSimManager.Instance.FetchConfig(Action<GameSimConfigResponse>) gets configuration information and takes
+        // a callback you can use to modify game state based on the configuration of parameters for the run
+        //
+        // You'll need to call this method and provide a callback to initialize your state.
+        // GameSimConfigResponse has a number of methods to get the value of some parameter.
+        // In this example, the level is parameterized as a string, so you'll need to call the GetString(key) method on
+        // the response object.
+        //
+        // This application loads level data by calling the SetLevelSO(LevelSO) method on the Match3 object.
+        // Read the level from the game sim manager, find it in your levelList, and pass along the appropriate level to
+        // your match3 instance.
 
-        // 5. Add event handler when winning/losing the game
-        match3.OnWin += Match3_OnWin;
-        match3.OnOutOfMoves += Match3_OnOutOfMoves;
+        // TODO: Add event handler when winning/losing the game
+        // Match3 provides event handlers OnWin and OnOutOfMoves.
     }
 
     private void Match3_OnWin(object sender, System.EventArgs e) {
-        // TODO: 3a. Enable tracking metric Win_Moves_Used when the game is won
-        // Reference: https://docs.unity3d.com/Packages/com.unity.simulation.games@0.4/manual/ImplementationGuide.html#step-4-enable-metrics-tracking
-        // Game Simulation uses a counter to track metrics. You can set, increase, and reset a counter's value at any point of your game code.
-        // When the the bot won the game, we want to get the number of moves used from "match3.GetUsedMoveCount()"
-        // and set the value to a counter named "Win_Moves_Used"
-        // You can set a counter's value by calling GameSimManager.Instance.SetCounter("counterName", counterValue);
+        // TODO: Enable tracking metric Win_Moves_Used when the game is won
+        // GameSimulation manages result data in 'counters'. They have an integral value type, and you may have any number
+        // of them.
+        //
+        // GameSimManager.Instance.SetCounter(string name, long value)
 
-        // TODO: 3b. Call method EndGameSimulation()
+        // TODO: Gracefully exit
     }
 
     private void Match3_OnOutOfMoves(object sender, System.EventArgs e) {
-        // TODO: 4a. Enable tracking metric Lose when the game is lost
-        // Reference: https://docs.unity3d.com/Packages/com.unity.simulation.games@0.4/manual/ImplementationGuide.html#step-4-enable-metrics-tracking
+        // TODO: Enable tracking metric Lose when the game is lost
+        // As above, you'll want to use SetCounter to track losing.
 
-        // TODO: 4b. Call method EndGameSimulation()
+        // TODO: Gracefully exit
     }
 
-    /* // TODO: 2a. Uncomment this method
-    private void OnFetchConfig(GameSimConfigResponse gameSimConfigResponse) {
-        // TODO: 2b. Load string type parameter "level" for grid search and set it to a string variable "levelName"
-        // Reference: https://docs.unity3d.com/Packages/com.unity.simulation.games@0.4/manual/ImplementationGuide.html#step-3-load-parameters-for-grid-search
-        // Parameter values are stored in GameSimConfigResponse object.
-        // You can access the individual variable by calling GameSimConfigResponse.Get[parameter type]("parameter name");
-
-        LevelSO loadLevel = null;
-
-        foreach (LevelSO levelSO in levelList) {
-            if (levelSO.name == levelName) {
-                loadLevel = levelSO;
-                break;
-            }
-        }
-
-        if (loadLevel == null) {
-            Debug.Log("Could not find level with name: " + levelName);
-            loadLevel = levelList[0];
-        }
-
-        this.levelSO = loadLevel;
-        match3.SetLevelSO(loadLevel);
-    }*/
-
-    // 6. Gracefully end the simulation
+    // Gracefully end the simulation
     private void EndGameSimulation() {
+    // It's convenient for this to function in the expected manner in editor..
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #else
