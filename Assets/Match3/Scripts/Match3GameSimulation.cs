c6c6b11d2a4ab4b897d4809b71a9dea4b79b7baf
Shuo Diao <shuo@unity3d.com>
2020-12-08 18:40:58 -0800

    Add detailed instruction

--- Assets/Match3/Scripts/Match3GameSimulation.cs
+++ Assets/Match3/Scripts/Match3GameSimulation.cs
@@ -1,7 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//TODO: 0. Import Unity Game Simulation package
+//TODO: 0. Import Unity Game Simulation package by uncommenting the line below
+//using Unity.Simulation.Game
 
 public class Match3GameSimulation : MonoBehaviour {
 
@@ -12,6 +13,9 @@ public class Match3GameSimulation : MonoBehaviour {
 
     private void Awake() {
         // TODO: 1. Load parameters for grid search
+        // Reference: https://docs.unity3d.com/Packages/com.unity.simulation.games@0.4/manual/ImplementationGuide.html#step-3-load-parameters-for-grid-search
+        // To load the parameters, call GameSimManager.Instance.FetchConfig(Action<GameSimConfigResponse>) here.
+        // Action<GameSimConfigResponse> is a call back method, and we will name it OnFetchConfig, which will be implemented in TODO: 2a.
 
         // 5. Add event handler when winning/losing the game
         match3.OnWin += Match3_OnWin;
@@ -19,18 +23,29 @@ public class Match3GameSimulation : MonoBehaviour {
     }
 
     private void Match3_OnWin(object sender, System.EventArgs e) {
-        // TODO: 3a. Enable tracking metric levelSO.name_Win_Moves_Used when the game is won
-        // TODO: 3b. Call EndGameSimulation()
+        // TODO: 3a. Enable tracking metric Win_Moves_Used when the game is won
+        // Reference: https://docs.unity3d.com/Packages/com.unity.simulation.games@0.4/manual/ImplementationGuide.html#step-4-enable-metrics-tracking
+        // Game Simulation uses a counter to track metrics. You can set, increase, and reset a counter's value at any point of your game code.
+        // When the the bot won the game, we want to get the number of moves used from "match3.GetUsedMoveCount()"
+        // and set the value to a counter named "Win_Moves_Used"
+        // You can set a counter's value by calling GameSimManager.Instance.SetCounter("counterName", counterValue);
+
+        // TODO: 3b. Call method EndGameSimulation()
     }
 
     private void Match3_OnOutOfMoves(object sender, System.EventArgs e) {
-        // TODO: 4a. Enable tracking metric levelSO.name_Lose when the game is lost
-        // TODO: 4b. Call EndGameSimulation()
+        // TODO: 4a. Enable tracking metric Lose when the game is lost
+        // Reference: https://docs.unity3d.com/Packages/com.unity.simulation.games@0.4/manual/ImplementationGuide.html#step-4-enable-metrics-tracking
+
+        // TODO: 4b. Call method EndGameSimulation()
     }
 
-    /* // TODO: 2b. Uncomment this method
+    /* // TODO: 2a. Uncomment this method
     private void OnFetchConfig(GameSimConfigResponse gameSimConfigResponse) {
-        // TODO: 2a. Load parameter "level" for grid search and set it to a string variable "levelName"
+        // TODO: 2b. Load string type parameter "level" for grid search and set it to a string variable "levelName"
+        // Reference: https://docs.unity3d.com/Packages/com.unity.simulation.games@0.4/manual/ImplementationGuide.html#step-3-load-parameters-for-grid-search
+        // Parameter values are stored in GameSimConfigResponse object.
+        // You can access the individual variable by calling GameSimConfigResponse.Get[parameter type]("parameter name");
 
         LevelSO loadLevel = null;
 
